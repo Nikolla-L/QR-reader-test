971bf37c7ae91030396b160254fea8905af19060
Nika <nlukava2002@gmail.com>
2022-09-08 22:29:48 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+
+function App() {
+  const [data, setData] = useState('No result');
+  const [openQR, setOpenQR] = useState(false);
+
+  return (
+    <div className="App">
+      <button onClick={() => setOpenQR(!openQR)}>
+        Open
+      </button>
+      {
+        openQR &&
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+            }
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          style={{
+            width: '100%',
+            maxWidth: '500px',
+            maxHeight: '80vh'
+          }}
+        />
+      }
+      <p>{data}</p>
+    </div>
+  );
+}
+
+export default App;
